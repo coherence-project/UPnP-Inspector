113bca40651f02d097b25e5102277d71ab80b508
Hartmut Goebel <h.goebel@crazy-compilers.com>
2011-06-24 10:39:10 +0200

    upnp-inspector setup.py: read long description from README

--- setup.py
+++ setup.py
@@ -6,35 +6,15 @@ from upnp_inspector import __version__
 
 packages = find_packages()
 
+long_description = "\n\n".join([
+    open("README.txt").read(),
+    ])
+
 setup(
     name="UPnP-Inspector",
     version=__version__,
     description="""UPnP Device and Service analyzer""",
-    long_description="""UPnP-Inspector is an UPnP Device and Service analyzer,
-based on the Coherence DLNA/UPnP framework.
-
-Loosely modeled after the Intel UPnP Device Spy and the UPnP Test Tool.
-
-Features:
-
- * inspect UPnP devices, services, actions and state-variables
- * invoke actions on any service
- * extract UPnP device- and service-description xml-files
- * follow and analyze events
- * interact with well-known devices
-   * browse the ContentDirectory of an UPnP A/V MediaServer and inspect its containers and items
-   * control an UPnP A/V MediaRenderer
-
-This 0.2.2 - Let the Sunshine In - release includes
-
- * a control-window for UPnP A/V MediaRenderers
- * Drag 'n Drop functionality from the MediaServer Browse window to the MediaRenderer control
- * a 'Rediscover Devices' menu item
- * a 'Refreshing Container' context menu item and an overall refresh upon closing the MediaServer Browse window
- * support for dlna-playcontainer URIs
- * displaying all elements from a DIDLLite fragment
-
-""",
+    long_description=long_description,
     author="Frank Scholz",
     author_email='coherence@beebits.net',
     license = "MIT",
